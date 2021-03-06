062329ae785fcd344fc272f88b017df35d25a51c
Erick Celio <erickcelio@hotmail.com>
2021-03-06 20:23:30 -0300

    create component LinkWrapper and your test

---
+++ src/components/LinkWrapper/index.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+import * as S from './styles'
+
+type LinkWrapperProps = {
+  href: string
+  children: React.ReactNode
+}
+
+const LinkWrapper = ({ href, children }: LinkWrapperProps) => {
+  return (
+    <S.Wrapper>
+      <Link href={href}>{children}</Link>
+    </S.Wrapper>
+  )
+}
+
+export default LinkWrapper
