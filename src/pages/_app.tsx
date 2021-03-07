de03f531670be8138e2492fd0d8a9c5d56155aed
Erick Celio <erickcelio@hotmail.com>
2021-03-07 12:49:52 -0300

    add nprogress to improve ux at loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import NextNProgress from 'nextjs-progressbar'
 
 import GlobalStyles from 'styles/global'
 
@@ -23,6 +24,12 @@ function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <GlobalStyles />
+      <NextNProgress
+        color="#e20e8d"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <Component {...pageProps} />
     </>
   )
